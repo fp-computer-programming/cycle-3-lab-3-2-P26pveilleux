92cc9e71484547f9d1065dc7764be97d834f60a5
P26pveilleux <144148685+P26pveilleux@users.noreply.github.com>
2023-09-26 10:43:02 -0400

    i did it

--- Lab_3.2.py
+++ Lab_3.2.py
@@ -4,11 +4,17 @@ Copy the following examples into a Python file one at a time.
 Think about what kind of error you think it will throw before running the code. 
 When you have run one line, be sure to comment it before adding your next line.
 After you have commented out a line, add a comment listing what error type occured.
-	
- 	int(a)
-	int('a')
-	'a' + 2
-	import date
-	print("I'm a happy camper!)
-
 """
+
+int(a)
+ # name error 
+int('a')
+ # type error 
+'a' + 2
+#type error 
+import date
+#module not found error 
+print("I'm a happy camper!)
+#syntax error  
+
+
